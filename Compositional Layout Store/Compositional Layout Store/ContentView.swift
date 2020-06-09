51de7e10a1d3c6c533074a8a54ad3614aa7fdbe4
Kevin Torres Martínez <44516784+KevinTMtz@users.noreply.github.com>
2020-06-08 20:14:30 -0500

    Compositional Layout.

---
+++ Compositional Layout Store/Compositional Layout Store/ContentView.swift
@@ -0,0 +1,185 @@
+//
+//  ContentView.swift
+//  Compositional Layout Store
+//
+//  Created by Kevin Torres on 08/06/20.
+//  Copyright © 2020 Kevin Torres Martínez. All rights reserved.
+//
+
+import SwiftUI
+
+class TechController: UICollectionViewController {
+    init() {
+        super.init(collectionViewLayout: TechController.createLayout())
+    }
+    
+    static func createLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout{ (sectionNumber, env) -> NSCollectionLayoutSection? in
+            
+            if sectionNumber == 0 {
+                // Section 0
+                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+                // item.contentInsets.trailing = 1
+                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(225)), subitems: [item])
+                
+                let section = NSCollectionLayoutSection(group: group)
+                
+                section.orthogonalScrollingBehavior = .paging
+                
+                return section
+            } else if sectionNumber == 1 {
+                // Section 2
+                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(0.25), heightDimension: .absolute(150)))
+                item.contentInsets.trailing = 16
+                item.contentInsets.bottom = 16
+                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .estimated(500)), subitems: [item])
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets.leading = 16
+                
+                section.boundarySupplementaryItems = [
+                    .init(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(50)), elementKind: categoryHeaderId, alignment: .topLeading)
+                ]
+                
+                return section
+            } else if sectionNumber == 2 {
+                // Section 2
+                let item = NSCollectionLayoutItem.init(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+                item.contentInsets.trailing = 32
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(0.85), heightDimension: .absolute(125)), subitems: [item])
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .continuous
+                section.contentInsets.leading = 16
+                return section
+            } else {
+                // Section 3
+                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(0.5), heightDimension: .absolute(250)))
+                item.contentInsets.trailing = 16
+                item.contentInsets.bottom = 16
+                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .estimated(500)), subitems: [item])
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets.leading = 16
+                
+                section.boundarySupplementaryItems = [
+                    .init(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(50)), elementKind: categoryHeaderId, alignment: .topLeading)
+                ]
+                
+                return section
+            }
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+        return header
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let controller = UIViewController()
+        controller.view.backgroundColor = indexPath.section == 0 ? .yellow : .blue
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    let headerId = "headerId"
+    static let categoryHeaderId = "categoryHeaderId"
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 3
+        } else if section == 1 {
+            return 8
+        } else if section == 2 {
+            return 4
+        } else {
+            return 5
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .init(red: 142.0/255, green: 68.0/255, blue: 173.0/255, alpha: 1.0)
+        return cell
+    }
+    
+    private let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .black
+        navigationItem.title = "Online Store"
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.register(Header.self, forSupplementaryViewOfKind: TechController.categoryHeaderId, withReuseIdentifier: headerId)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class Header: UICollectionReusableView {
+    let label = UILabel()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        label.text = "Categories"
+        label.font = UIFont.preferredFont(forTextStyle: .body, compatibleWith: UITraitCollection(legibilityWeight: .bold))
+        addSubview(label)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        label.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// Wrapper to implement my view controller
+struct ViewControllerWrapper: UIViewControllerRepresentable {
+    typealias UIViewControllerType = TechController
+
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ViewControllerWrapper>) -> ViewControllerWrapper.UIViewControllerType {
+        return TechController()
+    }
+
+    func updateUIViewController(_ uiViewController: ViewControllerWrapper.UIViewControllerType, context: UIViewControllerRepresentableContext<ViewControllerWrapper>) {
+        //
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Container().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct Container: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> UIViewController {
+            return UINavigationController(rootViewController: TechController())
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            
+        }
+        
+        typealias UIViewControllerType = UIViewController
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ViewControllerWrapper()
+    }
+}
---
+++ Compositional Layout Store/README.md
@@ -0,0 +1,6 @@
+# Compositional layout
+Compositional layout made with SwiftUI.
+
+<p align="center">
+  <img width="300" height="auto" src="screenShot.jpeg">
+</p>
