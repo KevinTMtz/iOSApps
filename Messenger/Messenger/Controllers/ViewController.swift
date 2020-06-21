002b39b79959208ad637848e562c6aa5f225ffe3
Kevin Torres Martínez <44516784+KevinTMtz@users.noreply.github.com>
2020-06-21 18:48:42 -0500

    Messenger App: GUI.

---
+++ Messenger/Messenger/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Kevin Torres on 21/06/20.
+//  Copyright © 2020 Kevin Torres Martínez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Messenger/README.md
@@ -0,0 +1,6 @@
+# Messenger
+Pending description.
+
+<p align="center">
+  <img width="300" height="auto" src="screenShot1.jpeg">
+</p>
