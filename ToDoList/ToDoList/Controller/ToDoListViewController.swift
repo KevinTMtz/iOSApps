74e58da80402f5e4764c0b3098f02befbc15cfe8
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-07-03 17:09:18 -0500

    ToDoList: Save & load data with NSCoder.

--- ToDoList/ToDoList/Controller/ToDoListViewController.swift
+++ ToDoList/ToDoList/Controller/ToDoListViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    var itemArray = ["Learn Tensorflow", "Create ToDoList", "Improve Gunned Down"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let savedItems = defaults.array(forKey: Cons.savedArray) as? [String] {
-            itemArray = savedItems
-        }
+        loadData()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -30,7 +30,11 @@ class ToDoListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: Cons.toDoViewCell, for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
@@ -38,14 +42,11 @@ class ToDoListViewController: UITableViewController {
     // MARK: - Table View Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        saveData()
     }
 
     // MARK: - Add new items
@@ -56,12 +57,12 @@ class ToDoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
         
         let addAction = UIAlertAction(title: "Add", style: .default) { (action) in
+            let newItem = Item()
+            newItem.title = textField.text!
             
-            self.itemArray.append(textField.text!)
+            self.itemArray.append(newItem)
             
-            self.defaults.set(self.itemArray, forKey: Cons.savedArray)
-            
-            self.tableView.reloadData()
+            self.saveData()
         }
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .default)
@@ -75,5 +76,33 @@ class ToDoListViewController: UITableViewController {
         alert.addAction(cancelAction)
         
         present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - Model Manipulation Methods
+    
+    func saveData() {
+        do {
+            let encoder = PropertyListEncoder()
+            
+            do {
+                let data = try encoder.encode(self.itemArray)
+                try data.write(to: dataFilePath!)
+            } catch {
+                print("Error encoding item array \(error)")
+            }
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadData() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder =  PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array \(error)")
+            }
+        }
     }
 }
---
+++ ToDoList/ToDoList/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ToDoList
+//
+//  Created by Kevin Torres on 03/07/20.
+//  Copyright © 2020 Kevin Torres Martínez. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
