6a2b3fd7394f23555a534c3eefef72df6ab4a97a
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-07-17 15:20:38 -0500

    Calculator: UI & README.

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Kevin Torres on 17/07/20.
+//  Copyright © 2020 Kevin Torres Martínez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Calculator/README.md
@@ -0,0 +1,10 @@
+# Calculator
+Minimalist design.
+
+<p align="center">
+  <img width="60" height="auto" style="border-radius: 20%;" src="Calculator/Assets.xcassets/AppIcon.appiconset/1024.png">
+</p>
+
+<p align="center">
+  <img width="300" height="auto" src="screenShot.jpeg">
+</p>
