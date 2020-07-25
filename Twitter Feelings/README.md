0c90273d5969da7cab0d2765878be9fed2b85f0b
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-07-25 12:19:23 -0500

    Twitter Feelings: UI & README.

---
+++ Twitter Feelings/README.md
@@ -0,0 +1,12 @@
+# Twitter Feelings
+Identifier of the average sentiment of a user's tweets or about a topic.
+
+Implementation of a Core ML model made and trained by me with Create ML.
+
+<p align="center">
+  <img width="60" height="auto" style="border-radius: 20%;" src="Twitter%20Feelings/Assets.xcassets/AppIcon.appiconset/1024.png">
+</p>
+
+<p align="center">
+  <img width="300" height="auto" src="screenShot1.jpg">
+</p>
---
+++ Twitter Feelings/Twitter Feelings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter Feelings
+//
+//  Created by Kevin Torres on 25/07/20.
+//  Copyright © 2020 Kevin Torres Martínez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var feelingsLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func predictPressed(_ sender: UIButton) {
+    
+    }
+}
+
