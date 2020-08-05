779510a6419f81586cc7a1ca9a7abfb30a61f892
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-08-04 21:16:39 -0500

    Roll Dices AR: Horizontal plane detection.

--- Roll Dices AR/AR Roll Dices/ViewController.swift
+++ Roll Dices AR/AR Roll Dices/ViewController.swift
@@ -17,40 +17,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
         // Set the view's delegate
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-        let cubeMaterial = SCNMaterial()
-        cubeMaterial.diffuse.contents = UIColor.purple
-        cube.materials = [cubeMaterial]
-        
-        let sphere = SCNSphere(radius: 0.3)
-        let sphereMaterial = SCNMaterial()
-        sphereMaterial.diffuse.contents = UIImage(named: "art.scnassets/jupiter.jpg")
-        sphere.materials = [sphereMaterial]
-        
-        let node1 = SCNNode()
-        node1.position = SCNVector3(x: -0.25, y: 0.1, z: -0.5)
-        node1.geometry = cube
-        
-        let node2 = SCNNode()
-        node2.position = SCNVector3(x: 0.25, y: 0.1, z: -0.5)
-        node2.geometry = sphere
-        
-        sceneView.scene.rootNode.addChildNode(node1)
-        sceneView.scene.rootNode.addChildNode(node2)
-        
         sceneView.autoenablesDefaultLighting = true
         
-//        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//        let cubeMaterial = SCNMaterial()
+//        cubeMaterial.diffuse.contents = UIColor.purple
+//        cube.materials = [cubeMaterial]
 //
-//        // Set the scene to the view
-//        sceneView.scene = scene
+//        let sphere = SCNSphere(radius: 0.3)
+//        let sphereMaterial = SCNMaterial()
+//        sphereMaterial.diffuse.contents = UIImage(named: "art.scnassets/jupiter.jpg")
+//        sphere.materials = [sphereMaterial]
+//
+//        let node1 = SCNNode()
+//        node1.position = SCNVector3(x: -0.25, y: 0.1, z: -0.5)
+//        node1.geometry = cube
+//
+//        let node2 = SCNNode()
+//        node2.position = SCNVector3(x: 0.25, y: 0.1, z: -0.5)
+//        node2.geometry = sphere
+//
+//        sceneView.scene.rootNode.addChildNode(node1)
+//        sceneView.scene.rootNode.addChildNode(node2)
+        
+        // Create a new scene
+        let diceScene = SCNScene(named: "art.scnassets/Dice.scn")!
+        
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+        
+            diceNode.position = SCNVector3(0, 0, -0.1)
+            sceneView.scene.rootNode.addChildNode(diceNode)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -59,6 +64,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
+        configuration.planeDetection = .horizontal
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -68,6 +75,29 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor {
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            let planeNode = SCNNode()
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            let gridMaterial = SCNMaterial()
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            
+            plane.materials = [gridMaterial]
+            
+            planeNode.geometry = plane
+            
+            node.addChildNode(planeNode)
+        } else {
+            return
+        }
     }
 
     // MARK: - ARSCNViewDelegate
