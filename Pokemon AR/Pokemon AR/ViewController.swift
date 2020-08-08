8329cdbf379c70fb6d34a7f0ecbedcbae62bb79d
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-08-08 01:19:06 -0500

    Pokemon AR: Full functionality.

---
+++ Pokemon AR/Pokemon AR/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  Pokemon AR
+//
+//  Created by Kevin Torres on 07/08/20.
+//  Copyright © 2020 Kevin Torres Martínez. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        sceneView.autoenablesDefaultLighting = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARImageTrackingConfiguration()
+
+        if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main) {
+            configuration.trackingImages = imageToTrack
+            configuration.maximumNumberOfTrackedImages = 2
+        }
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            let plane = SCNPlane(
+                width: imageAnchor.referenceImage.physicalSize.width,
+                height: imageAnchor.referenceImage.physicalSize.height)
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.0)
+            
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -Float.pi / 2
+            
+            node.addChildNode(planeNode)
+            
+            if let pokeScene = SCNScene(named: "art.scnassets/\(imageAnchor.referenceImage.name ?? "eevee").scn") {
+                if let pokeNode = pokeScene.rootNode.childNodes.first {
+                    pokeNode.eulerAngles.x = Float.pi / 2
+                    
+                    planeNode.addChildNode(pokeNode)
+                }
+            }
+        }
+        
+        return node
+    }
+}
---
+++ Pokemon AR/README.md
@@ -0,0 +1,15 @@
+# Pokemon AR
+Pokemon cards recognition with ARKit.
+
+<p align="center">
+  <img width="60" height="auto" style="border-radius: 20%;" src="Pokemon%20AR/Assets.xcassets/AppIcon.appiconset/1024.png">
+</p>
+
+<p align="center">
+  <img width="70%" height="auto" src="screenShot1.jpeg">
+</p>
+
+<p align="center">
+  <img width="70%" height="auto" src="screenShot2.jpeg">
+</p>
+
