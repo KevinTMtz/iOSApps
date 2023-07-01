1aec47a2e4ad8f31cbc7113039563649ba9f8181
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2023-06-30 23:56:15 -0600

    Hello World App.

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Kevin Torres Martínez on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
